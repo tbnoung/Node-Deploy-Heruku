766cc85be9527fda9605b2652dc8ccf65bd63485
Tbnoung <tbnoung@gmail.com>
2020-04-26 02:44:20 +0700

    setup app for Heroku

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const app = express()
+const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
   res.send('Hello World')
 })
 
-app.listen(3000, () => {
-  console.log('Start server at port 3000.')
+app.listen(port, () => {
+  console.log('Start server at port' + port)
 })
