bd6d64ce072272d17ce479f1aa3138cf6dce02fa
Tbnoung <tbnoung@gmail.com>
2020-04-26 02:51:04 +0700

    test path

--- src/app.js
+++ src/app.js
@@ -2,8 +2,11 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
-app.get('/', (req, res) => {
+app.get('', (req, res) => {
   res.send('Hello World')
+})
+app.get('/testpath', (req, res) => {
+  res.send('test Path')
 })
 
 app.listen(port, () => {
